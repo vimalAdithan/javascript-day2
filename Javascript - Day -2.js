456d28a8cf954e19f1c31381f8d4573c098118bb
Vimal <113934415+vimalAdithan@users.noreply.github.com>
2023-04-25 10:06:20 +0530

    Add files via upload

---
+++ Javascript - Day -2.js
@@ -0,0 +1,12 @@
+/*
+WINDOW
+It is an object
+Window is a space which holds the document .
+Window has several properties,it holds the properties of the frame.
+Windows represents the page.
+
+DOCUMENT
+Document is an object
+It holds several properties to make the window or the web page dynamic.
+Document comes under the window object.
+*/
